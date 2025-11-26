ba22b78731959d26a43c3117b609ddb34ce0242c
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2025-11-25 18:37:35 -0600

    change height

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "flex-start",
-        minHeight: "100vh",
+        height: "100vh",
         padding: "8px",
         gap: "8px",
       }}
